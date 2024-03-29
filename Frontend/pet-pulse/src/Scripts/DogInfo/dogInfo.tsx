3c52975c29db6979923b5fb7ba8792ddf3c78ec1
MistreanuEmanela <mistreanu.emanuela@gmail.com>
2024-03-29 19:14:42 +0200

    dog info front

--- Frontend/pet-pulse/src/Scripts/DogInfo/dogInfo.tsx
+++ Frontend/pet-pulse/src/Scripts/DogInfo/dogInfo.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styles from "./DogInfo.module.css";
-import Navbar from '../NavBars/SimpleNavBar';
-import Logo from '../Logo/logo';
+import Navbar from '../NavBars/NavBar';
 interface Dog {
     name: string;
     general: string;
@@ -14,27 +13,18 @@ interface Dog {
     friendship: string;
     care: string;
 }
-function addLineBreaks(text: string): string {
-    const breakAfter = [".", ";", "\n"];
-
-    for (const char of breakAfter) {
-        text = text.split(char).join(char + "\n");
-    }
-
-    return text;
-}
 
 const DogInfo: React.FC<{}> = () => {
     const [dog, setDog] = useState<Dog | null>(null);
     const [dogPicture, setDogPicture] = useState<string | null>(null);
     const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
-    const id = 347;
-    const name = "German Shepherd Dog.png";
+    const id = 61;
+    const name = "Akita";
 
     useEffect(() => {
         fetch(`http://localhost:8082/dog/${id}`, {
             headers: {
-                Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlbWEiLCJpYXQiOjE3MTA5NTg3NzcsImV4cCI6MTcxMTMxODc3N30.ifErpqHCq4-3A85zGeloraEzpR43WK8chDxRmBDsIOMHYV9vPzhOcz9cPJsd8RIuhGDMN7x64IdpRsZF1JUa0A'
+                Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlbWEiLCJpYXQiOjE3MTE3MjIzNDcsImV4cCI6MTcxMjA4MjM0N30.weZPK-yXdRvnSgqKoMrkvAhabym3Xxc36rpyrtCb2A75TVup5hbgLlEc_-GFVrNAlbqfhnutvZjvnw36L4xbEQ'
             }
         })
             .then(response => response.json())
@@ -43,7 +33,7 @@ const DogInfo: React.FC<{}> = () => {
 
 
         const myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlbWEiLCJpYXQiOjE3MTA5NTg3NzcsImV4cCI6MTcxMTMxODc3N30.ifErpqHCq4-3A85zGeloraEzpR43WK8chDxRmBDsIOMHYV9vPzhOcz9cPJsd8RIuhGDMN7x64IdpRsZF1JUa0A");
+        myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlbWEiLCJpYXQiOjE3MTE3MjIzNDcsImV4cCI6MTcxMjA4MjM0N30.weZPK-yXdRvnSgqKoMrkvAhabym3Xxc36rpyrtCb2A75TVup5hbgLlEc_-GFVrNAlbqfhnutvZjvnw36L4xbEQ");
 
         fetch(`http://localhost:8082/dog/picture/${name}`, {
             method: "GET",
@@ -62,47 +52,99 @@ const DogInfo: React.FC<{}> = () => {
     const handleCategoryClick = (category: string) => {
         setSelectedCategory(category);
         console.log(dog);
-        };
+        setTimeout(() => { // Delay scrolling by 2 seconds
+            const element = document.getElementById('category');
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth', block: 'end' }); 
+            }
+        }, 500); // 2000 milliseconds = 2 seconds
+    };
 
     return (
-        <><div className={styles.navbar}>
-            <Logo size={"50%"}></Logo>
-            <div className={styles.center}></div>
-            <Navbar></Navbar>
-        </div><div className={styles.dog_info}>
+        <div className={styles.body} id='category'>
+            <Navbar pagename="" />
+            <div className={styles.dog_info}>
                 {dog && (
-                    <div className={styles.dog_details}>
-                        <div className={styles.left_container}>
-                            {dogPicture && (
-                                <>
-                                    <div className={styles.dog_img}>
-                                        <img className={styles.image} src={dogPicture} alt="Dog" />
-                                    </div>
-                                    <div className={styles.name}>{dog.name}</div>
-                                </>
-                            )}
-                        </div>
-                        <div className={styles.right_container}>
-                            <div className="button-container">
-                                <button onClick={() => handleCategoryClick("general")}>General</button>
-                                <button onClick={() => handleCategoryClick("health")}>Health</button>
-                                <button onClick={() => handleCategoryClick("feeding")}>Feeding</button>
-                                <button onClick={() => handleCategoryClick("personality")}>Personality</button>
-                                <button onClick={() => handleCategoryClick("size")}>Size</button>
+                    <>
+                        <div className={styles.dog_details}>
+                            <div className={styles.left_container}>
+                                {dogPicture && (
+                                    <>
+                                        <div className={styles.dog_img}>
+                                            <img className={styles.image} src={dogPicture} alt="Dog" />
+                                        </div>
+                                        <div className={styles.name}>{dog.name}</div>
+                                    </>
+                                )}
                             </div>
-                            <div className={styles.display_container}>
-                                {selectedCategory === "general" && <p>{dog.general}</p>}
+                            <div className={styles.right_container}>
+                                <div className={styles.title}> General Info</div>
+                                <div className={styles.content}>{dog.general}</div>
+                            </div>
+                        </div>
+                        <div className={styles.second_part_info} >
+                            <div className={styles.button_container}>
+                                <button className={selectedCategory === "care" ? `${styles.button_care} ${styles.button_care_pressed}` : styles.button_care}
+
+                                    onClick={() => handleCategoryClick("care")}>
+                                    <div className={styles.button_care_image}> </div>
+                                    <div className={styles.button_name}>Care</div>
+                                </button>
+                                <button className={selectedCategory === "health" ? `${styles.button_care} ${styles.button_care_pressed}` : styles.button_care}
+                                    onClick={() => handleCategoryClick("health")}>
+                                    <div className={styles.button_health_image}> </div>
+                                    <div className={styles.button_name}>Health</div>
+                                </button>
+
+
+                                <button className={selectedCategory === "feeding" ? `${styles.button_care} ${styles.button_care_pressed}` : styles.button_care}
+                                    onClick={() => handleCategoryClick("feeding")}>
+                                    <div className={styles.button_food_image}> </div>
+                                    <div className={styles.button_name}>Fiding</div>
+                                </button>     
+
+                                <button className={selectedCategory === "personality" ? `${styles.button_care} ${styles.button_care_pressed}` : styles.button_care}
+                                    onClick={() => handleCategoryClick("personality")}>
+                                    <div className={styles.button_personality_image}> </div>
+                                    <div className={styles.button_name}>Personality</div>
+                                </button>  
+
+                                <button className={selectedCategory === "size" ? `${styles.button_care} ${styles.button_care_pressed}` : styles.button_care}
+                                    onClick={() => handleCategoryClick("size")}>
+                                    <div className={styles.button_size_image}> </div>
+                                    <div className={styles.button_name}>Size</div>
+                                </button> 
+                              
+                              
+                                <button className={selectedCategory === "coat" ? `${styles.button_care} ${styles.button_care_pressed}` : styles.button_care}
+                                    onClick={() => handleCategoryClick("coat")}>
+                                    <div className={styles.button_coat_image}> </div>
+                                    <div className={styles.button_name}>Coat</div>
+                                </button>  
+
+                                <button className={selectedCategory === "frendly" ? `${styles.button_care} ${styles.button_care_pressed}` : styles.button_care}
+                                    onClick={() => handleCategoryClick("frendly")}>
+                                    <div className={styles.button_frendly_image}> </div>
+                                    <div className={styles.button_name}>Friendship</div>
+                                </button>  
+                            </div>
+                            <div className={styles.display_container2} >
                                 {selectedCategory === "health" && <p>{dog.health}</p>}
                                 {selectedCategory === "feeding" && <p>{dog.feeding}</p>}
                                 {selectedCategory === "personality" && <p>{dog.personality}</p>}
                                 {selectedCategory === "size" && <p>{dog.size}</p>}
+                                {selectedCategory === "coat" && <p>{dog.coat}</p>}
+                                {selectedCategory === "frendly" && <p>{dog.friendship}</p>}
+                                {selectedCategory === "care" && <p>{dog.care}</p>}
                                 {selectedCategory === ""}
                             </div>
                         </div>
-                    </div>
+                    </>
                 )}
-            </div></>
+            </div>
+        </div>
     );
+
 }
 
 export default DogInfo;
